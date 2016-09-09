7a274c277f6340a41a30806f313beb1e4a1d162e
James Frank <jfrankdev90@gmail.com>
2016-09-09 00:37:33 -0700

    Adjusted password strength algo

--- js/index.js
+++ js/index.js
@@ -2,76 +2,103 @@ document.passwordForm.password.focus();
 
 $(document).ready(function() {
     var result = $("#password-strength");
+   
   
     $('#password').keyup(function(){
+
         $(".bar-text").html(checkStrength($('#password').val()))
+       
     })  
  
+
     function checkStrength(password){
  
-    //initial strength
-    var strength = 0
+   //initial strength
+    var strength = 0;
     
     if (password.length == 0) {
-        result.removeClass()
         document.getElementById("normal").style.visibility = "hidden";
         document.getElementById("medium").style.visibility = "hidden";
-        return ''
+        document.getElementById("medium2").style.visibility = "hidden";
+        document.getElementById("strong").style.visibility = "hidden";
+        document.getElementById("verystrong").style.visibility = "hidden";
     }
     //if the password length is less than 7, return message.
 
+    //
     
-    if (password.length < 7) {
-        result.removeClass()
-        result.addClass('normal')
 
-        document.getElementById("normal").style.visibility = "visible";
-
-        return 'Normal'
-    }
-
-    else {
-
-              document.getElementById("normal").style.visibility = "hidden";
-  
-    }
  
     //length is ok, lets continue.
  
-    //if length is 8 characters or more, increase strength value
-    if (password.length > 9) strength += 1
+    //if length is 8 characters or more, increase strength value, problematic line
+    if (password.length > 0) {strength += 1};
+
  
     //if password contains both lower and uppercase characters, increase strength value
-    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/) || password.length >= 3)  {strength += 1};
  
     //if it has one special character, increase strength value
-    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  {strength += 1};
+
+    //if it has one number, increase strength value
+    if (password.match(/([0-9])/))  {strength += 1};
  
     //if it has two special characters, increase strength value
-    if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,",%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+    if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,",%,&,@,#,$,^,*,?,_,~])/)) {strength += 2};
  
     //now we have calculated strength value, we can return messages
  
     //if value is less than 2
 
-   if (strength ===0) {
-        result.removeClass()
-        document.getElementById("medium").style.visibility = "hidden";
-        return ''
-    }    
+       
     //if the password length is less than 7, return message.
-    if (strength <= 2) {
-        result.removeClass()
-        document.getElementById("medium").style.visibility = "visible";
-        return ''
+    if (strength == 1) {
+        
+        document.getElementById("medium").style.visibility = "hidden";
+        document.getElementById("medium2").style.visibility = "hidden";
+        document.getElementById("strong").style.visibility = "hidden";
+        document.getElementById("verystrong").style.visibility = "hidden";
+        document.getElementById("normal").style.visibility = "visible";
     }
 
-    if (strength >= 3) {
-        result.removeClass()
-        document.getElementById("medium").style.visibility = "hidden";
-        return ''
+    if (strength == 2) {
+        
+        document.getElementById("normal").style.visibility = "hidden";
+        document.getElementById("medium2").style.visibility = "hidden";
+        document.getElementById("strong").style.visibility = "hidden";
+        document.getElementById("verystrong").style.visibility = "hidden";
+        document.getElementById("medium").style.visibility = "visible";
     }
+
+    if (strength == 3) {
+        document.getElementById("normal").style.visibility = "hidden";    
+        document.getElementById("medium").style.visibility = "hidden";
+        document.getElementById("strong").style.visibility = "hidden";
+        document.getElementById("verystrong").style.visibility = "hidden";
+        document.getElementById("medium2").style.visibility = "visible";
+    }
+
+    if (strength == 4) {
+        document.getElementById("medium2").style.visibility = "hidden";
+        document.getElementById("normal").style.visibility = "hidden";        
+        document.getElementById("medium").style.visibility = "hidden";
+        document.getElementById("verystrong").style.visibility = "hidden";
+        document.getElementById("strong").style.visibility = "visible";
+    }
+
+    if (strength > 4) {
+  
+        document.getElementById("strong").style.visibility = "hidden";
+        document.getElementById("normal").style.visibility = "hidden";        
+        document.getElementById("medium2").style.visibility = "hidden";
+        document.getElementById("medium").style.visibility = "hidden";
+        document.getElementById("verystrong").style.visibility = "visible";
+    }
+
+    console.log(strength);
 } 
+
 
 
 });
